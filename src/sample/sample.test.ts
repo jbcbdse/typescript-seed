6b222dc084de11e0cca7cdf58f701488f0e15708
jon <jonbarnett@gmail.com>
2019-08-11 22:09:12 -0500

    Fix sample module and unit test

--- src/sample/sample.test.ts
+++ src/sample/sample.test.ts
@@ -2,6 +2,6 @@ import { doubler } from './sample';
 
 describe('doubler', () => {
   it('should return the input number plus itself', () => {
-    expect(doubler(2)).toBe(4);
+    expect(doubler(3)).toBe(6);
   });
 });
